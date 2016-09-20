bbb4e53fc256f49b458b905458eb0480ab946c23
FeHHix <ifenix0@gmail.com>
2016-09-20 15:46:16 +0300

    The END.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var my_news = [
 	{
 		author: 'Саша Печкин',
@@ -15,6 +17,8 @@ var my_news = [
 		bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
 	}
 ];
+
+window.ee = new EventEmitter();
 
 var Article = React.createClass({
 	propTypes: {
@@ -101,9 +105,21 @@ var Add = React.createClass({
 	},
 	onBtnClickHandler: function(e) {
 		e.preventDefault();
+		var textEl = ReactDOM.findDOMNode(this.refs.text);
+
 		var author = ReactDOM.findDOMNode(this.refs.author).value;
 		var text = ReactDOM.findDOMNode(this.refs.text).value;
-		alert(author + '\n' + text);
+
+		var item = [{
+			author: author,
+			text: text,
+			bigText: '...'
+		}];
+
+		window.ee.emit('News.add', item);
+
+		textEl.value = '';
+		this.setState({textIsEmpty: true});
 	},
 	onFieldChange: function(fildName, e) {
 		if (e.target.value.trim().length > 0)
@@ -146,7 +162,7 @@ var Add = React.createClass({
 					onClick={this.onBtnClickHandler}
 					ref='alert_button'
 					disabled={agreeNotChecked || authorIsEmpty || textIsEmpty}>
-					Показать alert
+					Добавить новость
 				</button>
 			</form>
 		);
@@ -164,12 +180,28 @@ var Add = React.createClass({
 });*/
 
 var App = React.createClass({
+	getInitialState: function() {
+		return {
+			news: my_news
+		};
+	},
+	componentDidMount: function() {
+		var self = this;
+		window.ee.addListener('News.add', function(item) {
+			var nextNews = item.concat(self.state.news);
+			self.setState({news: nextNews});
+		});
+	},
+	componentWillUnmount: function() {
+		window.ee.removeListener('News.add');
+	},
 	render: function() {
+		console.log('render');
 		return (
 			<div className="app">
-				<h3>Новости</h3>
 				<Add />
-				<News data={my_news}/>
+				<h3>Новости</h3>
+				<News data={this.state.news}/>
 				{/*<Comments />*/}
 			</div>
 		);
